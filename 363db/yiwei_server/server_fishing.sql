ba31d23772d353ad032ace3c3cf0e4e7d57aaf3a
xmflswood <774898896@qq.com>
2026-03-12 23:39:39 +0800

    钓鱼概率

--- 363db/yiwei_server/server_fishing.sql
+++ 363db/yiwei_server/server_fishing.sql
@@ -27,9 +27,9 @@ CREATE TABLE `server_fishing` (
 -- ----------------------------
 INSERT INTO `server_fishing` VALUES ('41296', '鲷鱼', '1000000', '600000', '1', '2');
 INSERT INTO `server_fishing` VALUES ('41297', '鲑鱼', '1000000', '600000', '1', '2');
-INSERT INTO `server_fishing` VALUES ('41301', '发红光的鱼', '1000000', '450000', '1', '2');
-INSERT INTO `server_fishing` VALUES ('41302', '发绿光的鱼', '1000000', '450000', '1', '2');
-INSERT INTO `server_fishing` VALUES ('41303', '发蓝光的鱼', '1000000', '450000', '1', '2');
+INSERT INTO `server_fishing` VALUES ('41301', '发红光的鱼', '1000000', '9000', '1', '2');
+INSERT INTO `server_fishing` VALUES ('41302', '发绿光的鱼', '1000000', '9000', '1', '2');
+INSERT INTO `server_fishing` VALUES ('41303', '发蓝光的鱼', '1000000', '9000', '1', '2');
 INSERT INTO `server_fishing` VALUES ('41304', '发白光的鱼', '1000000', '450000', '1', '2');
 INSERT INTO `server_fishing` VALUES ('41306', '破碎的戒指', '1000000', '150000', '1', '2');
 INSERT INTO `server_fishing` VALUES ('41307', '破碎的项链', '1000000', '150000', '1', '2');
@@ -40,4 +40,4 @@ INSERT INTO `server_fishing` VALUES ('21053', '泡水的盔甲', '1000000', '150
 INSERT INTO `server_fishing` VALUES ('21054', '泡水的手套', '1000000', '150000', '1', '2');
 INSERT INTO `server_fishing` VALUES ('21055', '泡水的靴子', '1000000', '150000', '1', '2');
 INSERT INTO `server_fishing` VALUES ('21056', '泡水的盾牌', '1000000', '150000', '1', '2');
-INSERT INTO `server_fishing` VALUES ('41252', '珍奇的乌龟', '1000000', '50000', '1', '2');
+INSERT INTO `server_fishing` VALUES ('41252', '珍奇的乌龟', '1000000', '9000', '1', '2');
